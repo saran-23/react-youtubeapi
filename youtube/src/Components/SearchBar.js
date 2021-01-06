a496cf8b83fd74026f3a94d196a8ad12fd6b1fdc
saran-23 <romansaran446@gmail.com>
2021-01-06 15:30:03 +0530

    onInputChange-displays input txt.. onFormSubmit-prevent from refresh

--- youtube/src/Components/SearchBar.js
+++ youtube/src/Components/SearchBar.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = {term: ''};
+
+// used to enter in the input field 
+   onInputChange =event => {
+        this.setState({term: event.target.value
+        });
+    };
+
+    onFormSubmit = (event) => {
+        event.preventDefault(); //prevent from refeshing the page
+    };
     render(){
         return (
-            <div>
-                SearchBar
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                <div className="field">
+                    <label>Video Search</label>
+                    <input type="text"
+                     value={this.state.term} 
+                     onChange={this.onInputChange}
+                     />
+                </div>
+                </form>
             </div>
         );
      }
