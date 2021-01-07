2eaa12518967ac709085b06782a4d4fea1597358
saran-23 <romansaran446@gmail.com>
2021-01-07 13:35:15 +0530

    everything Works :)

--- youtube/src/Components/App.js
+++ youtube/src/Components/App.js
@@ -5,14 +5,22 @@ import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
  class App extends React.Component {
-     state = { videos: [], selectedVideo: null }
+     state = { videos: [], selectedVideo: null };
+
+     componentDidMount() {
+        this.onTermSubmit('nikola tesla and its');
+     }
     onTermSubmit  = async term => {
   const response = await  youtube.get('/search', {
             params: {
                 q: term
             }
         });
-        this.setState({videos: response.data.items});
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+
+        });
   
     };
 
